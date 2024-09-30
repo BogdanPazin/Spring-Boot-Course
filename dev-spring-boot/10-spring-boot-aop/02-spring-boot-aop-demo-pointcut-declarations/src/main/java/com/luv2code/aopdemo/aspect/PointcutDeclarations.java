002e60d0ee2640977c51df696b28629be3765c00
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-09-30 19:21:02 +0200

    Update PointcutDeclarations.java

--- dev-spring-boot/10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/PointcutDeclarations.java
+++ dev-spring-boot/10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/PointcutDeclarations.java
@@ -3,8 +3,7 @@ package com.luv2code.aopdemo.aspect;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
-@Aspect // AKO IMAM SAMO POINTCUT EXPRESSIONS ONDA JE OVA ANOTACIJA OPCIONALNA
-        // MORA DA SE PISE AKO IMAM I ADVICE-OVE
+@Aspect
 
 public class PointcutDeclarations {
     @Pointcut("execution(public * com.luv2code.aopdemo.dao.*.*(..))")
@@ -22,7 +21,6 @@ public class PointcutDeclarations {
 
     }
 
-    // KOMBINACIJA POINTCUT DECLARATION-OVA, GDE CE SE IGNORISATI SAMO GETERI I SETERI
     @Pointcut("forDaoPackage() && !(forGetters() || forSetters())")
     public void excludeGetterAndSetter(){
 
