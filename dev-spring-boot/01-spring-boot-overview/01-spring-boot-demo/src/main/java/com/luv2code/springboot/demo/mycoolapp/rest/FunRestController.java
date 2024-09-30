88d705b21fd59dc9645cdb3c5959c20fd66900df
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-09-30 19:25:38 +0200

    Update FunRestController.java

--- dev-spring-boot/01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ dev-spring-boot/01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
-    // stavlja za / kao default da vraca tekst Hello World
     @GetMapping("/")
     public String sayHello(){
         return "Hello World!";
