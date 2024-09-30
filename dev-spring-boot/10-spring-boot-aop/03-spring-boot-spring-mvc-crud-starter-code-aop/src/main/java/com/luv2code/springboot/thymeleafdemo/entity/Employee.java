c1f88718370e4f3ba8d262aae1de344db9aba651
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-09-30 19:24:19 +0200

    Update Employee.java

--- dev-spring-boot/10-spring-boot-aop/03-spring-boot-spring-mvc-crud-starter-code-aop/src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
+++ dev-spring-boot/10-spring-boot-aop/03-spring-boot-spring-mvc-crud-starter-code-aop/src/main/java/com/luv2code/springboot/thymeleafdemo/entity/Employee.java
@@ -6,7 +6,6 @@ import jakarta.persistence.*;
 @Table(name="employee")
 public class Employee {
 
-    // define fields
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
@@ -21,8 +20,6 @@ public class Employee {
     @Column(name="email")
     private String email;
 
-
-    // define constructors
     public Employee() {
 
     }
@@ -32,8 +29,6 @@ public class Employee {
         this.lastName = lastName;
         this.email = email;
     }
-
-    // define getter/setter
 
     public int getId() {
         return id;
@@ -67,7 +62,6 @@ public class Employee {
         this.email = email;
     }
 
-    // define toString
     @Override
     public String toString() {
         return "Employee{" +
