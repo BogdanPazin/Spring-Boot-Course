09b901481900c0100a4385e1ce8f18eb918db79e
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-09-30 19:27:08 +0200

    Update FunRestController.java

--- dev-spring-boot/01-spring-boot-overview/03-actuator-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ dev-spring-boot/01-spring-boot-overview/03-actuator-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -6,19 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
-    // stavlja za / kao default da vraca tekst Hello World
     @GetMapping("/")
     public String sayHello(){
         return "Hello World!";
     }
 
-    // stavlja novi link za "workout"
     @GetMapping("/workout")
     public String getDailyWorkout(){
         return "Run a half 5km!";
     }
-
-    //novi link za "fortune"
+    
     @GetMapping("/fortune")
     public String getDailyFortune(){
         return "Today is your lucky day!";
