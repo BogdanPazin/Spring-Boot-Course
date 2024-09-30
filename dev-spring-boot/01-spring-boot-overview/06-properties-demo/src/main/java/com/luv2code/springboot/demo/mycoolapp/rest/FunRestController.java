1bdd53592d43b06e829650c3a0d1a3684b10fa92
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-09-30 19:30:10 +0200

    Update FunRestController.java

--- dev-spring-boot/01-spring-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ dev-spring-boot/01-spring-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -7,29 +7,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
-    // stavlja za / kao default da vraca tekst Hello World
     @GetMapping("/")
     public String sayHello(){
         return "Hello World!";
     }
 
-    // stavlja novi link za "workout"
     @GetMapping("/workout")
     public String getDailyWorkout(){
         return "Run a half 5km!";
     }
 
-    //novi link za "fortune"
     @GetMapping("/fortune")
     public String getDailyFortune(){
         return "Today is your lucky day!";
     }
 
-    //ubacujem podatke iz application.properties fajla
     @Value("${coach.name}") private String coachName;
     @Value("${team.name}") private String teamName;
 
-    //pravim novi endpoint
     @GetMapping("/teaminfo")
     public String getTeamInfo(){
         return "Coach: " + coachName + ", team name: " + teamName;
