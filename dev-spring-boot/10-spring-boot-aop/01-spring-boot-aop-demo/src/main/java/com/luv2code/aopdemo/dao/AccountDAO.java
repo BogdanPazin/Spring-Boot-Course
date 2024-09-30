fa349bda449e42c72dc8ce9a3761699263deb7a6
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-09-30 19:15:44 +0200

    Update AccountDAO.java

--- dev-spring-boot/10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
+++ dev-spring-boot/10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -3,7 +3,6 @@ package com.luv2code.aopdemo.dao;
 import com.luv2code.aopdemo.Account;
 
 public interface AccountDAO {
-    // OVDE PISEM BUSINESS METODE
     void addAccount(Account account, boolean vipFlag);
 
     boolean doWork();
