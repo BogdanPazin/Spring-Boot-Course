566b7182bd877bef7844057e0c3aa4ec1900b1f1
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-10-01 17:44:06 +0200

    Update EmployeeRepository.java

--- dev-spring-boot/04-spring-boot-rest-crud-api/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ dev-spring-boot/04-spring-boot-rest-crud-api/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -4,5 +4,5 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    // ovaj interface ce da sluzi kao DAO
+
 }
