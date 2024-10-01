aa040a340f5560f765631309f722aac4f77a5a8c
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-10-01 18:06:20 +0200

    Update Course.java

--- dev-spring-boot/09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/entity/Course.java
+++ dev-spring-boot/09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/entity/Course.java
@@ -16,28 +16,20 @@ public class Course {
     @Column(name = "title")
     private String title;
 
-    //podesio sam cascade tako da ako obrisem course, ne brisem instruktora koji je bio vezan za taj kurs
-    //vise kurseva moze biti povezano sa jednim instruktorom
     @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,
             CascadeType.PERSIST, CascadeType.REFRESH})
-    // ovo je naziv kolone u samoj tabeli course, kako bi course pronasao tacnog instruktora
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
 
-    @OneToMany(cascade = CascadeType.ALL, // SA OVIM CE SE OBRISATI I SVE RECENZIJE SA KURSOM, AKO OBRISEM KURS
-            fetch = FetchType.LAZY) // SA OVIM CE SE RECENZIJE UCITATI PO ZAHTEVU A NE AUTOMATSKI
+    @OneToMany(cascade = CascadeType.ALL, 
+            fetch = FetchType.LAZY)
 
-    // ZBOG TOGA STO JE UNIDIRECTIONAL ASOCIJACIJA, U KLASI course MORA DA SE OZNACI
-    // DA HIBERNATE ZNA DA PRONADJE ODGOVARAJUCE RECENZIJE ZA TRAZENI KURS
     @JoinColumn(name = "course_id")
     private List<Review> reviews = new ArrayList<>();
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.DETACH, CascadeType.MERGE,  // JEDAN KURS MOZE IMATI VISE STUDENATA
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.DETACH, CascadeType.MERGE, 
             CascadeType.PERSIST, CascadeType.REFRESH})
-    @JoinTable(name = "course_student", // POVEZUJEM SA JOIN TABELOM U BAZI
-
-            // OVDE JE OSTVARENA VEZA SA JEDNE STRANE, KURS -> STUDENT
-            // INVERZNA KOLONA OMOGUCAVA DA HIBERNATE NADJE ODGOVARAJUCE STUDENTE ZA TRAZENI KURS
+    @JoinTable(name = "course_student",
             joinColumns = @JoinColumn(name = "course_id"),
             inverseJoinColumns = @JoinColumn(name = "student_id"))
     private List<Student> students = new ArrayList<>();
