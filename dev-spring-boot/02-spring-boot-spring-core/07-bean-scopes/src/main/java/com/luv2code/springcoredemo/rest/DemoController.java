21147d53c9ff7af29697cc9ffa869eaa7a425a7a
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-10-01 17:36:16 +0200

    Update DemoController.java

--- dev-spring-boot/02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
+++ dev-spring-boot/02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -11,13 +11,9 @@ public class DemoController {
     private Coach myCoach;
     private Coach anotherCoach;
 
-    // anotacija @autowired ukazuje springu da moze ovo da se koristi za injection
-    // ako imam samo jedan konstruktor onda je anotacija @autowired opcionalna
-
     @Autowired
     public DemoController(@Qualifier("tennisCoach") Coach myCoach,
                           @Qualifier("tennisCoach") Coach anotherCoach) {
-        //ako koristim primary anotaciju, ne moram da imam qualifier u listi parametara za konstruktor
 
         this.myCoach = myCoach;
         this.anotherCoach = anotherCoach;
