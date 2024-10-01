72b81f0ff2c5704069527481fd5e2256237cc260
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-10-01 18:03:50 +0200

    Update AppDAOImplement.java

--- dev-spring-boot/09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAOImplement.java
+++ dev-spring-boot/09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAOImplement.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository // ovo je da bi spring nasao komponentu tokom skeniranja
+@Repository
 public class AppDAOImplement implements AppDAO {
 
     private EntityManager entityManager;
@@ -30,8 +30,6 @@ public class AppDAOImplement implements AppDAO {
     @Override
     public Instructor findInstructorById(int theId) {
 
-        // ovo ce da vrati i instructor details jer je default ponasanje @OneToOne
-        // postavljeno na eager
         return entityManager.find(Instructor.class, theId);
     }
 
@@ -42,19 +40,15 @@ public class AppDAOImplement implements AppDAO {
 
         List<Course> courses = instructor.getCourses();
 
-        // uklonim iz kursa tog instruktora
         for(Course course : courses){
             course.setInstructor(null);
         }
 
-        // ovo ce onda da obrise i instructor details objekat jer je cascade postavljen na ALL
-        // i nece kurseve jer sam tog instruktora izbacio sa svih njegovih kurseva
         entityManager.remove(instructor);
     }
 
     @Override
     public InstructorDetail findInstructorDetailById(int theId) {
-        // ovo sad vraca instructorDetail ali sa tim vraca i odgovarajuceg instructor-a
         return entityManager.find(InstructorDetail.class, theId);
     }
 
@@ -63,20 +57,15 @@ public class AppDAOImplement implements AppDAO {
     public void deleteInstructorDetailById(int theId) {
         InstructorDetail instructorDetail = entityManager.find(InstructorDetail.class, theId);
 
-        // uklanjam povezan objekat
-        // prekidam bidirection vezu
         instructorDetail.getInstructor().setInstructorDetail(null);
 
-        // sa ovim ce da se obrise i odgovarajuci instructor
         entityManager.remove(instructorDetail);
     }
 
     @Override
     public List<Course> findCoursesByInstructorId(int theId) {
-                                                                // IZ NEKOG RAZLOGA MORA OVAKAV QUERY DA BUDE, MSM NA VELIKA I MALA SLOVA
         TypedQuery<Course> typedQuery = entityManager.createQuery("from Course where instructor.id = :data", Course.class);
-                // IMENOVANI PARAMETAR U QUERY MORA DA IMA : ISPRED SAMOG IMENA, A SAMA VREDNOST SE POSLE DODELJUJE
-
+        
         typedQuery.setParameter("data", theId);
 
         List<Course> courses = typedQuery.getResultList();
@@ -87,10 +76,9 @@ public class AppDAOImplement implements AppDAO {
     @Override
     public Instructor findInstructorByIdJoinFetch(int theId) {
         TypedQuery<Instructor> typedQuery = entityManager.createQuery(
-                // PAZI!!! MORA NA KRAJU LINIJE DA SE UDARI SPACE, OSIM POSLEDNJE
                 "select instructor from Instructor instructor " +
-                "JOIN FETCH instructor.courses " + // JOIN FETCH je slicno kao EAGER loading, dobijam instruktora i sve kurseve za njega
-                "JOIN FETCH instructor.instructorDetail " + //OVA LINIJA MOZE DA SE OBRISE, ISTI JE ISPIS SAMO JE SMANJEN BROJ QUERY-JA KOJI SE IZVODI
+                "JOIN FETCH instructor.courses " + 
+                "JOIN FETCH instructor.instructorDetail " + 
                         "where instructor.id = :data", Instructor.class);
 
         typedQuery.setParameter("data", theId);
@@ -103,7 +91,6 @@ public class AppDAOImplement implements AppDAO {
     @Override
     @Transactional
     public void updateInstructor(Instructor instructor) {
-        // MERGE metod azurira vec postojeci objekat
         entityManager.merge(instructor);
     }
 
@@ -129,7 +116,6 @@ public class AppDAOImplement implements AppDAO {
     @Override
     @Transactional
     public void saveCourse(Course course) {
-        // SACUVACE KURS I SVE RECENZIJE ZA TAJ KURS
         entityManager.persist(course);
     }
 
