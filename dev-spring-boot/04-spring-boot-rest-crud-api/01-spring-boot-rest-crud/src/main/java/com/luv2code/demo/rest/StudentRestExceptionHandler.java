1403ba0c8fcc44eebd0181aa3109acd515009134
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-10-01 17:40:43 +0200

    Update StudentRestExceptionHandler.java

--- dev-spring-boot/04-spring-boot-rest-crud-api/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestExceptionHandler.java
+++ dev-spring-boot/04-spring-boot-rest-crud-api/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestExceptionHandler.java
@@ -5,14 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-// ova klasa ce da mi sluzi za global exception handling
-// svi handleri ce biti na jednom mestu a ne rasprostranjeni u rest controller-ima
 @ControllerAdvice
 public class StudentRestExceptionHandler {
 
     @ExceptionHandler
-    // ova metoda ce da uhvati exception ako se baci
-    // gde je genericki tip StudentErrorResponse zapravo body samog teksta koji se vraca kao rezultat
     public ResponseEntity<StudentErrorResponse> catchException(StudentNotFoundException exception){
         StudentErrorResponse response = new StudentErrorResponse();
         response.setStatus(HttpStatus.NOT_FOUND.value());
@@ -20,7 +16,6 @@ public class StudentRestExceptionHandler {
         response.setTimeStamp((int) System.currentTimeMillis());
 
         ResponseEntity<StudentErrorResponse> responseEntity = new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-        // prvi parametar konstuktora je telo poruke, a drugi je kod statusa
         return responseEntity;
     }
 
