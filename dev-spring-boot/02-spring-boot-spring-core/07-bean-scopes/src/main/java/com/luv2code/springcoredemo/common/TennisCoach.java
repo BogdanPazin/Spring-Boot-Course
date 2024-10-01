f60a3fa309443a76cb7361f8ef8d7fcf333209ac
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-10-01 17:36:45 +0200

    Update TennisCoach.java

--- dev-spring-boot/02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
+++ dev-spring-boot/02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/luv2code/springcoredemo/common/TennisCoach.java
@@ -5,8 +5,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) //sa ovim tipom bean-a ce se pri svakom inject-u praviti novi bean
-                                                //samim tim razlicite injekcije nece pokazivati na isti bean
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) 
 public class TennisCoach implements Coach{
 
     public TennisCoach() {
