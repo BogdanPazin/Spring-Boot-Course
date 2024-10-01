9cd7d44f1dfc23756cb52542066cee1b825e3ca1
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-10-01 17:44:56 +0200

    Update EmployeeRepository.java

--- dev-spring-boot/04-spring-boot-rest-crud-api/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ dev-spring-boot/04-spring-boot-rest-crud-api/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -4,9 +4,5 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-// sa ovom anotacijom menjam default-ni naziv koji daje Spring Data REST za url
-// umesto default-nog /employees, sada ce da bude /members
-//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    // ovaj interface ce da sluzi kao DAO
 }
