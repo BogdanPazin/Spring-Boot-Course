69b2a159e1bbb98870ee133788eb3096b6183c0f
BogdanPazin <168831585+BogdanPazin@users.noreply.github.com>
2024-10-01 17:33:58 +0200

    Update CricketCoach.java

--- dev-spring-boot/02-spring-boot-spring-core/05-primary/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ dev-spring-boot/02-spring-boot-spring-core/05-primary/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -3,10 +3,6 @@ package com.luv2code.springcoredemo.common;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-// anotacija @Component nad klasom oznacava obicnu java klasu kao spring bean
-// odnosno oznacava je kao klasu koju moze da koristi i upravlja spring
-// a i takodje dopusta da se ova klasa koristi za dependancy injection
-
 @Component
 public class CricketCoach implements Coach{
     @Override
